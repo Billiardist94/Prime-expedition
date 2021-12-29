bd6ae3eb2a8e6dbcb3a3eab5f1c364b1fd0d8180
Your Name <you@example.com>
2021-12-29 12:47:37 +0300

    first commit

--- script.js
+++ script.js
@@ -1,90 +1,31 @@
-// ___Swipers___
+// ___Scroll Up-btn___
+const arrowUp = document.querySelector('.scrollup');
+const arrowUpDisplay = getComputedStyle(arrowUp).display;
+function arrowUpGo(e) {
+    e.preventDefault;
+    arrowUp.classList.add('anim__arrowUp');
+    setTimeout(() => {
+        arrowUp.classList.remove('anim__arrowUp')
+    },1000)
+}
+arrowUp.addEventListener('click', arrowUpGo);
 
-const swiper1 = new Swiper(".swiper1", {
-    speed: 1000,
-    slidesPerView: 1,
-    spaceBetween: 20,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: true,
-    },
-});
-
-const swiper2 = new Swiper(".swiper2", {
-    speed: 1000,
-    slidesPerView: 1,
-    spaceBetween: 20,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: true,
-    },
-});
-
-const swiper3 = new Swiper(".swiper3", {
-    speed: 1000,
-    slidesPerView: 4,
-    spaceBetween: 20,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: true,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
-        dynamicBullets: true,
-      },
-      grabCursor: true,
-      
-    // Responsive breakpoints
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-        },
-        // when window width is >= 360px
-        360: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-            },
-        // when window width is >= 425px
-        425: {
-        slidesPerView: 2,
-        spaceBetween: 30
-        },
-        // when window width is >= 576px
-        576: {
-        slidesPerView: 3,
-        spaceBetween: 20
-        },
-        // when window width is >= 768px
-        768: {
-        slidesPerView: 3,
-        spaceBetween: 20
-        },
-        // when window width is >= 992px
-        992: {
-        slidesPerView: 4,
-        spaceBetween: 20
-        },
-    },
-});
+$(document).ready(function(){
+         
+    $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+    $('.scrollup').fadeIn();
+    } else {
+    $('.scrollup').fadeOut();
+    }
+    });
+     
+    $('.scrollup').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 0);
+    return false;
+    });
+     
+    });
 
 // ___Menu-Burger___
 
@@ -115,42 +56,3 @@ window.addEventListener('scroll', (e) => {
 links.forEach((el) => {
     el.addEventListener('click', closeBurger);
 })
-
-// ___Paralax-effect___
-
-window.onload = () => {
-    document.querySelector('.content').style.backgroundAttachment = "fixed";
-    window.onscroll = function () {
-    document.querySelector('.content').style.backgroundPosition = "0px " + (0 + (Math.max(document.documentElement.scrollTop, document.body.scrollTop) / 8)) + "px";
-    }
-};
-
-// ___Anim-rocket___
-
-const rocket = document.querySelector('.scrollup');
-const rocketDispaly = getComputedStyle(rocket).display;
-function rocketGo(e) {
-    e.preventDefault;
-    rocket.classList.add('anim__rocket');
-    setTimeout(() => {
-        rocket.classList.remove('anim__rocket')
-    },1000)
-}
-rocket.addEventListener('click', rocketGo);
-
-$(document).ready(function(){
-         
-    $(window).scroll(function(){
-    if ($(this).scrollTop() > 100) {
-    $('.scrollup').fadeIn();
-    } else {
-    $('.scrollup').fadeOut();
-    }
-    });
-     
-    $('.scrollup').click(function(){
-    $("html, body").animate({ scrollTop: 0 }, 0);
-    return false;
-    });
-     
-    });
